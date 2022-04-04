e6b4fe6fa231e366f2c4baf5326886c96bab0c12
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-04 23:52:09 +0530

    Made some minor changes

--- random_gifs/src/useGif.js
+++ random_gifs/src/useGif.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
