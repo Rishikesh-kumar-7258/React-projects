b36a2557638036c36d6f7763a1b63fd11b33c640
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-06 16:08:56 +0530

    almost done

--- superheros/src/components/super_hero.jsx
+++ superheros/src/components/super_hero.jsx
@@ -1,25 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useGetSuperHero from '../useGetSuperHero'
 
 const SuperHero = () => {
-    const [superHero, loading] = useGetSuperHero("A-Bomb");
+    const [id, setid] = useState("1")
+    const [superHero, loading] = useGetSuperHero(id);
 
     useEffect(() => {
         console.log(superHero);
     }, [superHero]);
 
-    if (loading) return "loading ..."
+    if (loading || !superHero) return "loading..."
 
     return (
         <div className="container">
-                <div className="row">
-                    <div className="col-4">
-                        <h1>{superHero.name}</h1>
-                    </div>
-                    <div className="col-4">
-                        <img src={superHero.images.md} alt={superHero.name} />
-                    </div>
+            <div className="row">
+                <div className="col-12">
+                    <input type="text" className="input-group" value={id} onChange={e => setid(e.target.value)} />
                 </div>
+            </div>
+            <div className="row">
+                <div className="col-4">
+                    <h1>{superHero.name}</h1>
+                </div>
+                <div className="col-4">
+                    <img src={superHero.images.md} alt={superHero.name} />
+                </div>
+            </div>
         </div>
     )
 }
--- superheros/src/useGetSuperHero.js
+++ superheros/src/useGetSuperHero.js
@@ -1,19 +1,31 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const useGetSuperHero = (name) => {
+const useGetSuperHero = (id) => {
     const [superHero, setSuperHero] = useState(null)
     const [loading, setloading] = useState(true)
-    const [allSuperhero, setallSuperhero] = useState(null)
+
+    if (id === null || id === '') id = '1';
 
     useEffect(() => {
-        async function fetchData() {
-            const response = await fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json`)
-            const data = await response.json()
-            setallSuperhero(data);
-        }
-        fetchData()
 
-    }, [])
+        let cancel;
+        setloading(true);
+        axios({
+            method: 'get',
+            url : `https://akabab.github.io/superhero-api/api/id/${id}.json`,
+            cancelToken : new axios.CancelToken(c => cancel = c)
+        })
+        .then(res => {
+            setSuperHero(res.data)
+        }).catch(e => {
+            if (axios.isCancel(e)) return;
+        })
+
+        setloading(false)
+        return () => cancel()
+
+    }, [id])
 
     return [superHero, loading]
 }
